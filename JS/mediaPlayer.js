5c45f2d70e643fa081a126978077818ce7d01b17
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-09-05 15:49:57 -0600

    Plugin AutoPlay.js

--- JS/mediaPlayer.js
+++ JS/mediaPlayer.js
@@ -4,7 +4,9 @@
 // import MediaPlayer from './plugins/AutoPlay.js';
 
     const video = document.querySelector('video');
-    const player = new MediaPlayer({el: video, plugins: [],});
+    const player = new MediaPlayer({el: video, plugins: [
+        new AutoPlay 
+    ],});
 
     const button = document.querySelector('button')
     button.onclick = () => player.togglePlay();
